889d729270ba5254c8ff51ab625649c9c8037cdf
Eddie Stenbacka <71704857+wstenbacka@users.noreply.github.com>
2026-06-15 14:23:31 +0300

    Add user URLs

---
+++ backend/users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'', UserViewSet, basename='user')
+urlpatterns = router.urls
